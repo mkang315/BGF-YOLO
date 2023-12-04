4df1aec068fa2dcb624ddc7d306bfb33191a66d9
mkang315 <53328998+mkang315@users.noreply.github.com>
2023-12-04 12:37:30 +0800

    Update predict.py

--- yolo/bgf/detect/predict.py
+++ yolo/bgf/detect/predict.py
@@ -1,4 +1,4 @@
-# BGFG-YOLO based on Ultralytics YOLOv8x 8.0.109 object detection model with same license, AGPL-3.0 license
+# BGF-YOLO based on Ultralytics YOLOv8x 8.0.109 object detection model with same license, AGPL-3.0 license
 
 import torch
 
